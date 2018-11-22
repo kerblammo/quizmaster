4c37b6a256dbe6ca267cec21cd26755b30b3645e
kerblammo <31718829+kerblammo@users.noreply.github.com>
2018-11-22 15:41:32 -0400

    Attempting Ignore
    Want to set connectionManager to an ignored file to preserve confidentiality of connection string

--- db/connectionManger.php
+++ db/connectionManger.php
@@ -2,7 +2,7 @@
 class ConnectionManager {
 
     public function connect_db() {
-        $db = new PDO("[REDACTED - CX STRING]", "[REDACTED - USER]", "[REDACTED - PASSWORD]");
+        $db = new PDO("[REDACTED - CX STRING]", "[REDACTED - USERNAME]", "[REDACTED - PASSWORD]");
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $db;
     }
