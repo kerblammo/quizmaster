1ec117e4a7a191abcd80d5cb3204419a10a4c746
kerblammo <31718829+kerblammo@users.noreply.github.com>
2018-11-22 15:57:23 -0400

    Adding connection manager
    Those folks on stack overflow do NOT know what they're about

---
+++ db/connectionManager.php
@@ -0,0 +1,9 @@
+<?php
+class ConnectionManager {
+    public function connect_db() {
+        $db = new PDO("[REDACTED - CX STRING]", "[REDACTED - USER]", "[REDACTED - PASSWORD]");
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $db;
+    }
+}
+
