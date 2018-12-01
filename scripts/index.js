fd6f3c202dbb0ba453675d312769d5d7f05434c2
ahaines6 <38072387+ahaines6@users.noreply.github.com>
2018-12-01 11:29:05 -0400

    12-01

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 window.onload = function () {
 //user is saved in local storage as a user object
 //{"id":1,"permissionId":1,"username":"PeterAdam","password":"quizzmaster","deactivated":0}
-document.querySelector("#loginOpt").addEventListener("click", handleDisplayLogin);
+
     if (localStorage.getItem("userLoggedIn") !== null) {
 
         document.querySelector("#loginOpt").innerHTML = "Log Out";
@@ -25,13 +25,5 @@ document.querySelector("#loginOpt").addEventListener("click", handleDisplayLogin
     document.querySelector("#loginOpt").addEventListener("click", handleDisplayLogin);
 }
 
-function handleDisplayLogin() {
-    if (document.querySelector("#loginOpt").innerHTML == "Log Out") {
-        localStorage.clear();
-        alert("Goodbye");
-      window.location.href="index.php";
-    }
-
-}
 
 
