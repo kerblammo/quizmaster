48d1c92e6b27e57701f5bd0bdc33a2a7d1267a6d
ahaines6 <38072387+ahaines6@users.noreply.github.com>
2018-11-30 21:48:54 -0400

    SearchQuizzes

--- scripts/search.js
+++ scripts/search.js
@@ -4,8 +4,9 @@ window.onload = function () {
     document.querySelector("#searchByResults").classList.add("hidden");
     document.querySelector("#leftRadio").addEventListener("click", searchByQuiz);
     document.querySelector("#rightRadio").addEventListener("click", searchByResults);
-    
-         
+     document.querySelector("#searchByQuizFilter").addEventListener("change", clearFields);
+
+
 
 
     if (localStorage.getItem("userLoggedIn") !== null) {
@@ -24,6 +25,9 @@ window.onload = function () {
     }
     document.querySelector("#loginOpt").addEventListener("click", handleDisplayLogin);
 }
+function clearFields(){
+    document.querySelector("#searchTermInput").value="";
+}
 
 function handleDisplayLogin() {
     if (document.querySelector("#loginOpt").innerHTML == "Log Out") {
@@ -36,44 +40,92 @@ function handleDisplayLogin() {
 
 
 function submitHandler() {
-   
-   var selectedSearch=document.querySelector("#searchByQuizFilter").value;
-  var searchValue=document.getElementById("searchTermInput").value;
-   console.log(searchValue);
-    if (selectedSearch=="id") {
-         var url = "quizmaster/quiz/" + searchValue;
+
+    var selectedSearch = document.querySelector("#searchByQuizFilter").value;
+    var searchValue = document.getElementById("searchTermInput").value;
+    console.log(searchValue);
+    if (selectedSearch == "id") {
+        var url = "quizmaster/quiz/" + searchValue;
+        getOneQuiz(url);
+
     }
-    if (selectedSearch=="tag") {
-        var url = "quizmaster/quiz/byTag" + searchValue;
+    if (selectedSearch == "tag") {
+        var url = "quizmaster/quiz/byTag/" + searchValue;
+        getMatchingQuizzes(url);
     }
-    if (selectedSearch=="tag") {
-        var url = "quizmaster/quiz/byName" + searchValue;
+    if (selectedSearch == "word") {
+        var url = "quizmaster/quiz/byName/" + searchValue;
+        getMatchingQuizzes(url);
     }
- 
 
-        var method = "GET";
+}
 
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function () {
-            if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
-                var resp = xmlhttp.responseText;
-                  console.log(resp);
-                if (resp !== "null") {
-                    console.log(resp);
+function getMatchingQuizzes(url) {
+    var method = "GET";
 
-                 
-                 
-
-
-
-                } else {
-                    alert("Sorry, please check user name and password")
-                }
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+            var resp = xmlhttp.responseText;
+            console.log(resp);
+            if (resp !== "null") {
+                console.log(resp);
+                showMatchinQuizzes(resp);
+            } else {
+                alert("Sorry, please check user name and password")
             }
-        };
-        xmlhttp.open(method, url, true);
-        xmlhttp.send();
+        }
+    };
+    xmlhttp.open(method, url, true);
+    xmlhttp.send();
+}
+function showMatchinQuizzes(resp) {
+    var data = JSON.parse(resp);
+    var html = "";
+    for (var i = 0; i < data.length; i++) {
 
+        var quizName = data[i].title;
+        var description = data[i].description;
+        html += "<div id='outputData' class='column25'><div id='searchBox'><div id='quizName'>" + quizName + "</div>";
+        html += "<div id='quizDescription'><p>" + description + "</p></div>";
+        html += "<div id='btnTakeQuiz'><button>Take Quiz</button></div></div></div></div>";
+    }
+
+    document.querySelector("#output").innerHTML = html;
+    console.log(data[0].id);
+}
+function getOneQuiz(url) {
+    var method = "GET";
+
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+            var resp = xmlhttp.responseText;
+            console.log(resp);
+            if (resp !== "null") {
+                console.log(resp);
+                showOneQuiz(resp);
+            } else {
+                alert("Sorry, please check user name and password")
+            }
+        }
+    };
+    xmlhttp.open(method, url, true);
+    xmlhttp.send();
+}
+function showOneQuiz(resp) {
+    var data = JSON.parse(resp);
+    var quizName = data.title;
+    var description = data.description;
+
+    var html = "<div id='outputData' class='column25'><div id='searchBox'><div id='quizName'>" + quizName + "</div>";
+    html += "<div id='quizDescription'><p>" + description + "</p></div>";
+    html += "<div id='btnTakeQuiz'><button>Take Quiz</button></div></div></div></div>";
+
+
+    document.querySelector("#output").innerHTML = html;
+    console.log(data.id);
+      document.querySelector("#searchByQuizFilter").addEventListener("change", clearFields);
 }
 function searchByQuiz() {
     var radChecker = document.querySelector("#radQuiz");
